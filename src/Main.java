2c555092b0e2751a7eac0fc0e5b714c5bd533203
Artnes <2023java@mail.ru>
2023-03-20 11:53:55 +0300

    Домашнее задание 4 - продолжение

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
     }
+    public static void task1() {
+        System.out.println("Задача 1");
+        byte age = 18;
+        if (age >= 18) {
+            System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний");}
+        if (age < 18) {
+            System.out.println("Если возраст человека равен " + age + ", то он не достиг совершеннолетия, нужно немного подождать");}
+    }
+    public static void task2() {
+        System.out.println("Задача 2");
+        byte airTemperature= 6;
+        if (airTemperature >= 5) {
+            System.out.println("На улице " + airTemperature + " градусов, можно идти без шапки");}
+        if (airTemperature < 5) {
+            System.out.println("На улице " + airTemperature + " градусов, нужно надеть шапку");}
+    }
+
 }
